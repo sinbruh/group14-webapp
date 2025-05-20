5cfcbfc549a20c2630e015466835ab907b65d1ea
Mats Bakketeig <mats.bakketeig@outlook.com>
2025-05-20 15:39:24 +0200

    Refactor configuration entity

--- backend/src/main/java/no/ntnu/project/group14/webapp/entities/Car.java
+++ backend/src/main/java/no/ntnu/project/group14/webapp/entities/Car.java
@@ -1,51 +1,60 @@
 package no.ntnu.project.group14.webapp.entities;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
 import java.util.LinkedHashSet;
 import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 /**
  * The Car class represents the entity for cars. Each car is further specified by a set of one or
  * more {@link Configuration configurations}.
  */
-@Entity(name = "car")
+@Entity
+@Table(name = "car")
 @Schema(description = "Car entity representing car")
 public class Car {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "car_id")
   @Schema(description = "Unique ID")
   private Long id;
 
+  @Column(name = "make")
   @Schema(description = "Car make")
   private String make;
 
+  @Column(name = "model")
   @Schema(description = "Car model")
   private String model;
 
+  @Column(name = "year")
   @Schema(description = "Car model release year")
   private int year;
 
-  @Schema(description = "Car configurations")
   @OneToMany(mappedBy = "car")
+  @JsonBackReference
+  @Schema(description = "Car configurations")
   private Set<Configuration> configurations = new LinkedHashSet<>();
 
   /**
-   * Constructs an instance of the Car class.
-   *
-   * <p>Empty constructor needed for JPA.</p>
+   * Constructor for the Car class. This default constructor is required by JPA.
    */
   public Car() {
     // Intentionally left blank
   }
 
   /**
-   * Constructs an instance of the Car class.
+   * Constructor for the Car class.
    *
    * @param make  The specified make
    * @param model The specified model
@@ -103,11 +112,12 @@ public class Car {
   }
 
   /**
-   * Returns true if the car is valid or false otherwise.
+   * Checks if car is valid.
    *
-   * @return True if the car is valid or false otherwise
+   * @return True if car is valid or false otherwise
    */
   public boolean isValid() {
-    return !this.make.isBlank() && !this.model.isBlank() && this.year >= 0;
+    return this.make != null && !this.make.isBlank() && this.model != null && !this.model.isBlank()
+        && this.year >= 0;
   }
 }
--- backend/src/main/java/no/ntnu/project/group14/webapp/entities/Configuration.java
+++ backend/src/main/java/no/ntnu/project/group14/webapp/entities/Configuration.java
@@ -1,67 +1,77 @@
 package no.ntnu.project.group14.webapp.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
 import java.util.LinkedHashSet;
 import java.util.Set;
 
 /**
- * The Configuration class represents the entity class for the configuration entity.
- * 
- * <p>The class uses JPA with annotations for ORM operations.</p>
- *
- * @author Group 4
- * @version v1.0 (2024.05.22)
+ * The Configuration class represents the entity for configurations. Configurations represent the
+ * different variations of a {@link Car car}. Each configuration is provided in the application
+ * through the distribution of {@link RentalObject rental objects}.
  */
-@Entity(name = "configuration")
-@Schema(
-    description = "A configuration entity, representing a specific car configuration that can be "
-                + "added to a car"
-)
+@Entity
+@Table(name = "configuration")
+@Schema(description = "Configuration entity representing a car variation")
 public class Configuration {
-  @Schema(description = "Unique ID")
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "configuration_id")
+  @Schema(description = "Unique ID")
   private Long id;
+
+  @Column(name = "name")
   @Schema(description = "Configuration name")
   private String name;
+
+  @Column(name = "fuel_type")
   @Schema(description = "Configuration fuel type")
   private String fuelType;
+
+  @Column(name = "transmission_type")
   @Schema(description = "Configuration transmission type")
   private String transmissionType;
+
+  @Column(name = "number_of_seats")
   @Schema(description = "Number of seats in the configuration")
   private int numberOfSeats;
-  @Schema(description = "Car the configuration belongs to")
-  @JsonIgnore
+
   @ManyToOne
+  @JsonManagedReference
+  @JoinColumn(name = "car_id")
+  @Schema(description = "Car the configuration belongs to")
   private Car car;
-  @Schema(description = "Extra features in the configuration")
+
   @OneToMany(mappedBy = "configuration")
+  @Schema(description = "Extra features in the configuration")
   private Set<ExtraFeature> extraFeatures = new LinkedHashSet<>();
+
+  @OneToMany(mappedBy = "configuration")
   @JsonManagedReference
   @Schema(description = "Rental objects distributing the configuration")
-  @OneToMany(mappedBy = "configuration")
-  private Set<Provider> rentalObjects = new LinkedHashSet<>();
+  private Set<RentalObject> rentalObjects = new LinkedHashSet<>();
 
   /**
-   * Constructs an instance of the Configuration class.
-   *
-   * <p>Empty constructor needed for JPA.</p>
+   * Constructor for the Configuration class. This default constructor is required by JPA.
    */
   public Configuration() {
     // Intentionally left blank
   }
 
   /**
-   * Constructs an instance of the Configuration class.
+   * Constructor for the Configuration class.
    *
    * @param name             The specified name
    * @param fuelType         The specified fuel type
@@ -85,30 +95,12 @@ public class Configuration {
   }
 
   /**
-   * Setter for ID.
-   *
-   * @param id The specified ID
-   */
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  /**
    * Getter for name.
    *
    * @return Name
    */
   public String getName() {
     return this.name;
-  }
-
-  /**
-   * Setter for name.
-   *
-   * @param name The specified name
-   */
-  public void setName(String name) {
-    this.name = name;
   }
 
   /**
@@ -121,30 +113,12 @@ public class Configuration {
   }
 
   /**
-   * Setter for fuel type.
-   *
-   * @param fuelType The specified fuel type
-   */
-  public void setFuelType(String fuelType) {
-    this.fuelType = fuelType;
-  }
-
-  /**
    * Getter for transmission type.
    *
    * @return Transmission type
    */
   public String getTransmissionType() {
     return this.transmissionType;
-  }
-
-  /**
-   * Setter for transmission type.
-   *
-   * @param transmissionType The specified transmission type
-   */
-  public void setTransmissionType(String transmissionType) {
-    this.transmissionType = transmissionType;
   }
 
   /**
@@ -157,30 +131,12 @@ public class Configuration {
   }
 
   /**
-   * Setter for number of seats.
-   *
-   * @param numberOfSeats The specified number of seats
-   */
-  public void setNumberOfSeats(int numberOfSeats) {
-    this.numberOfSeats = numberOfSeats;
-  }
-
-  /**
    * Getter for car.
    *
    * @return Car
    */
   public Car getCar() {
     return this.car;
-  }
-
-  /**
-   * Setter for car.
-   *
-   * @param car The specified car
-   */
-  public void setCar(Car car) {
-    this.car = car;
   }
 
   /**
@@ -193,30 +149,22 @@ public class Configuration {
   }
 
   /**
-   * Setter for extra features.
-   *
-   * @param extraFeatures The specified extra features
-   */
-  public void setExtraFeatures(Set<ExtraFeature> extraFeatures) {
-    this.extraFeatures = extraFeatures;
-  }
-
-  /**
    * Getter for rental objects.
    *
    * @return Rental objects
    */
-  public Set<Provider> getRentalObjects() {
+  public Set<RentalObject> getRentalObjects() {
     return this.rentalObjects;
   }
 
   /**
-   * Returns true if the configuration is valid or false otherwise.
+   * Checks if configuration is valid.
    *
-   * @return True if the configuration is valid or false otherwise
+   * @return True if configuration is valid or false otherwise
    */
   public boolean isValid() {
-    return !this.name.isBlank() && !this.fuelType.isBlank() && !this.transmissionType.isBlank()
-           && this.numberOfSeats > 0;
+    return this.name != null && !this.name.isBlank() && this.fuelType != null
+        && !this.fuelType.isBlank() && this.transmissionType != null
+        && !this.transmissionType.isBlank() && this.numberOfSeats > 0;
   }
 }
