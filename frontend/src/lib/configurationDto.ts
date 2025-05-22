3bf24224cccdaa939e9546418b610777b269798f
mathi <mathiaswestergaardjohansen@gmail.com>
2025-05-22 15:56:04 +0200

    created configurationDTo interface

---
+++ frontend/src/lib/configurationDto.ts
@@ -0,0 +1,9 @@
+export interface ConfigurationDto {
+  name: string;
+  make: string;
+  model: string;
+  numberOfSeats: number;
+  fuelType: string;
+  transmissionType: string;
+  price: number;
+}
