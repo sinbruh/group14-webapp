99079e5249cb987f2d49dd871d283709e87ade5e
Mats Bakketeig <matsbak@stud.ntnu.no>
2025-05-22 15:54:13 +0200

    Edit configuration DTO

--- backend/src/main/java/no/ntnu/project/group14/webapp/dto/ConfigurationDto.java
+++ backend/src/main/java/no/ntnu/project/group14/webapp/dto/ConfigurationDto.java
@@ -1,167 +1,109 @@
 package no.ntnu.project.group14.webapp.dto;
 
+import no.ntnu.project.group14.webapp.entities.Configuration;
 
 /**
- * The ConfigurationDto class represents the data transfer object (DTO) for receiving
- * configuration data. 
- *
- * @author Group 14
- * @version v1.0 (2025.05.22)
+ * The ConfigurationDto class represents the data transfer object (DTO) for
+ * {@link Configuration configurations}.
  */
 public class ConfigurationDto {
-    private String name;
-    private String make;
-    private String model;
-    private int numberOfSeats;
-    private String fuelType;
-    private String transmissionType;
-    private double price;
+  private String make;
+  private String model;
+  private String name;
+  private String fuelType;
+  private String transmissionType;
+  private int numberOfSeats;
+  private double price;
 
-    /**
-     * Constructor for the ConfigurationDto class.
-     * This constructor is used to create a ConfigurationDto object with the specified parameters.
-     * @param name the name of a car model
-     * @param make the make of a car
-     * @param model the model of a car
-     * @param numberOfSeats the number of seats in a car
-     * @param fuelType the fuel type of a car
-     * @param transmissionType the transmission type of a car
-     * @param price the price of a car
-     */
-    public ConfigurationDto(String name, String make, String model, int numberOfSeats,
-                            String fuelType, String transmissionType, double price) {
-        this.name = name;
-        this.make = make;
-        this.model = model;
-        this.numberOfSeats = numberOfSeats;
-        this.fuelType = fuelType;
-        this.transmissionType = transmissionType;
-        this.price = price;
-    }
+  /**
+   * Constructor for the ConfigurationDto class.
+   * 
+   * @param make The specified car make
+   * @param model The specified car model
+   * @param name The specified name
+   * @param fuelType The specified fuel type
+   * @param transmissionType The specified transmission type
+   * @param numberOfSeats The specified number of seats
+   * @param price The specified lowest rental object price
+   */
+  public ConfigurationDto(
+    String make,
+    String model,
+    String name,
+    String fuelType,
+    String transmissionType,
+    int numberOfSeats,
+    double price
+  ) {
+    this.make = make;
+    this.model = model;
+    this.name = name;
+    this.fuelType = fuelType;
+    this.transmissionType = transmissionType;
+    this.numberOfSeats = numberOfSeats;
+    this.price = price;
+  }
 
-    /**
-     * Getter for name.
-     * 
-     * @return the specified name
-     */
-    public String getName() {
-        return name;
-    }
+  /**
+   * Getter for car make.
+   * 
+   * @return Car make
+   */
+  public String getMake() {
+    return make;
+  }
 
-    /**
-     * Setter for name.
-     * 
-     * @param name the specified name
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
+  /**
+   * Getter for car model.
+   * 
+   * @return Car model
+   */
+  public String getModel() {
+    return model;
+  }
 
-    /**
-     * Getter for make.
-     * 
-     * @return the specified make
-     */
-    public String getMake() {
-        return make;
-    }
+  /**
+   * Getter for name.
+   * 
+   * @return Name
+   */
+  public String getName() {
+    return name;
+  }
 
-    /**
-     * Setter for make.
-     * 
-     * @param make the specified make
-     */
-    public void setMake(String make) {
-        this.make = make;
-    }
+  /**
+   * Getter for fuel type.
+   * 
+   * @return Fuel type
+   */
+  public String getFuelType() {
+    return fuelType;
+  }
 
-    /**
-     * Getter for model.
-     * 
-     * @return the specified model
-     */
-    public String getModel() {
-        return model;
-    }
+  /**
+   * Getter for transmission type.
+   * 
+   * @return Transmission type
+   */
+  public String getTransmissionType() {
+    return transmissionType;
+  }
 
-    /**
-     * Setter for model.
-     * 
-     * @param model the specified model
-     */
-    public void setModel(String model) {
-        this.model = model;
-    }
+  /**
+   * Getter for number of seats.
+   * 
+   * @return Number of seats
+   */
+  public int getNumberOfSeats() {
+    return numberOfSeats;
+  }
 
-    /**
-     * Getter for number of seats.
-     * 
-     * @return number of seats
-     */
-    public int getNumberOfSeats() {
-        return numberOfSeats;
-    }
-
-    /**
-     * Setter for number of seats
-     * 
-     * @param numberOfSeats number of seats
-     */
-    public void setNumberOfSeats(int numberOfSeats) {
-        this.numberOfSeats = numberOfSeats;
-    }
-
-    /**
-     * Getter for fuel type.
-     * 
-     * @return fuel type
-     */
-    public String getFuelType() {
-        return fuelType;
-    }
-
-    /**
-     * Setter for fuel type.
-     * 
-     * @param fuelType fuel type
-     */
-    public void setFuelType(String fuelType) {
-        this.fuelType = fuelType;
-    }
-
-    /**
-     * Getter for transmission type.
-     * 
-     * @return transmission type
-     */
-    public String getTransmissionType() {
-        return transmissionType;
-    }
-
-    /**
-     * Setter for transmission type.
-     * 
-     * @param transmissionType transmission type
-     */
-    public void setTransmissionType(String transmissionType) {
-        this.transmissionType = transmissionType;
-    }
-
-    /**
-     * Getter for price.
-     * 
-     * @return price
-     */
-    public double getPrice() {
-        return price;
-    }
-
-    /**
-     * Setter for price
-     * 
-     * @param price price
-     */
-    public void setPrice(double price) {
-        this.price = price;
-    }
+  /**
+   * Getter for lowest rental object price.
+   * 
+   * @return Lowest rental object price
+   */
+  public double getPrice() {
+    return price;
+  }
 }
